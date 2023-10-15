3a02d5aa10bf78af401f27dc1724dcc8c16230c6
Lavensoft <46833976+lavensoft@users.noreply.github.com>
2023-10-15 22:15:12 +0700

    U Wallet Api

--- src/api/WalletApi.js
+++ src/api/WalletApi.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import Config from "../config/Config";
+
 export default class WalletApi {
    static async connect() {
       await window.phantom.solana.connect();
@@ -5,14 +8,45 @@ export default class WalletApi {
       let publicKey = window.phantom.solana.publicKey.toBase58();
       console.log(publicKey);
 
-      localStorage.setItem("@publicKey", publicKey);
+      localStorage.setItem(Config.SK_PUBLIC_KEY, publicKey);
    }
 
    static isConnected() {
       return window.phantom.solana.isConnected;
    }
 
-   static getBalance() {
+   static async getBalance() {
+      const walletAddress = localStorage.getItem(Config.SK_PUBLIC_KEY);
+      let res = await axios.get(
+         `${Config.SHYFT}/wallet/balance?network=${Config.NETWORK}&wallet=${walletAddress}`, 
+         {
+            headers: {
+               "x-api-key": Config.SHYFT_API_TOKEN
+            }
+         }
+      );
+   
+      return res.data?.result?.balance || 0;
+   }
 
+   static async getTransactionHistory() {
+      const walletAddress = localStorage.getItem(Config.SK_PUBLIC_KEY);
+      let res = await axios.get(
+         `${Config.SHYFT}/wallet/transaction_history?network=${Config.NETWORK}&wallet=${walletAddress}`, 
+         {
+            headers: {
+               "x-api-key": Config.SHYFT_API_TOKEN
+            }
+         }
+      );
+
+      return res.data?.result || [];
+   }
+
+   static async getWallet() {
+      let balance = await this.getBalance();
+      let transactionHistory = await this.getTransactionHistory();
+
+      console.log(transactionHistory);
    }
 }
--- src/config/Config.js
+++ src/config/Config.js
@@ -1,3 +1,8 @@
 export default class Config {
    static SHYFT_API_TOKEN = "FvtQ-DsN9H3YDIYw";
+   static SHYFT = "https://api.shyft.to/sol/v1";
+   static NETWORK = "devnet";
+
+   //STORAGE KEYS
+   static SK_PUBLIC_KEY = "@publicKey";
 }
