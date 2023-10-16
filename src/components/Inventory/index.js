60fa09b0801e8f4d9107dccf7c7c748698c84b22
Lavensoft <46833976+lavensoft@users.noreply.github.com>
2023-10-16 11:53:29 +0700

    U

--- src/components/Inventory/index.js
+++ src/components/Inventory/index.js
@@ -1,24 +1,36 @@
 import { Card } from "../Card";
 import "./styles.scss";
+import { Button } from "../Button";
 
-export const Inventory = ({ onItemSelect, walletData }) => {
+export const Inventory = ({ onItemSelect, walletData, onRefreshWallet, walletFetching }) => {
    return (
       <div className="inventory-container">
-         {
-            walletData?.nfts?.map((item, index) => {
-               if(!item.image_uri) return null;
+         <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+         }}>
+            <h3 className="mission__title" style={{ flex: 1 }}>Your Objects</h3>
+            <Button onClick={onRefreshWallet} loading={walletFetching}>Refresh Objects</Button>
+         </div>
 
-               return (
-                  <Card
-                     key={`in-nft-card-${index}`}
-                     name={item.name}
-                     image={item.image_uri}
-                     qty={1}
-                     onClick={() => onItemSelect(item)}
-                  />
-               )
-            })
-         }
+         <div className="inventory-container__container">
+            {
+               walletData?.nfts?.map((item, index) => {
+                  if(!item.image_uri) return null;
+
+                  return (
+                     <Card
+                        key={`in-nft-card-${index}`}
+                        name={item.name}
+                        image={item.image_uri}
+                        qty={1}
+                        onClick={() => onItemSelect(item)}
+                     />
+                  )
+               })
+            }
+         </div>
       </div>
    )
 }
